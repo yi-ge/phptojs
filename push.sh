0ee8e71cf78859cbf5bef770e6d0dce89383797b
yi-ge <a@wyr.me>
2017-12-26 18:59:16 +0800

    update - 2017-12-26 18:59:16

--- push.sh
+++ push.sh
@@ -3,10 +3,8 @@ if [ $1 ]; then
   git add .
   git commit -m $1
   git push origin master
-  git push gitee master
 else
   git add .
   git commit -m "update - `date '+%Y-%m-%d %T'`"
   git push origin master
-  git push gitee master
 fi
